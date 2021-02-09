8a5b52ccd3c742363a823489d45c227edeee686c
ayush mishra <ayushmishra0810@gmail.com>
2021-02-09 20:47:00 +0530

    Chat Inital Commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBSCRtA7md1PSPxk9bD9qtLfV2gyK54O6o",
+    authDomain: "chatapp-7de37.firebaseapp.com",
+    projectId: "chatapp-7de37",
+    storageBucket: "chatapp-7de37.appspot.com",
+    messagingSenderId: "1056453298393",
+    appId: "1:1056453298393:web:2de1b9baf747be6207d6c8",
+    measurementId: "G-JPDGCDXXHP"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBSCRtA7md1PSPxk9bD9qtLfV2gyK54O6o",
+    authDomain: "chatapp-7de37.firebaseapp.com",
+    projectId: "chatapp-7de37",
+    storageBucket: "chatapp-7de37.appspot.com",
+    messagingSenderId: "1056453298393",
+    appId: "1:1056453298393:web:2de1b9baf747be6207d6c8",
+    measurementId: "G-JPDGCDXXHP"
+  }
 };
 
 /*
