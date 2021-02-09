232301fd0e86929780d8e5f5228c9c4fe8dca0d8
ayush mishra <ayushmishra0810@gmail.com>
2021-02-10 01:11:37 +0530

    Updates

--- src/app/chat-room/chat-room.page.ts
+++ src/app/chat-room/chat-room.page.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat-room.page.scss'],
 })
 export class ChatRoomPage implements OnInit {
+  constructor() {}
+  user = 'Ayush';
+  chats = [
+    { name: 'Ayush', message: 'Hello KHushboo', time: 1612898487 },
+    { name: 'KHushboo', message: 'Hello Ayush', time: 1612898488 },
+     { name: 'KHushboo', message: 'Hello Ayush', time: 1612898488 },
+  ];
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
